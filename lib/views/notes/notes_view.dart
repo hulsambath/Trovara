15556c54d7f952d0a598ff6fd69046e5f6e93214
Sambath <hulsambath14@gmail.com>
2025-09-28 02:59:10 +0700

    Close #1 Custom pull refresh indicator

--- lib/views/notes/notes_view.dart
+++ lib/views/notes/notes_view.dart
@@ -4,6 +4,7 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:noteminds/core/base/view_model_provider.dart';
 import 'package:noteminds/views/notes/widgets/note_card.dart';
+import 'package:noteminds/widgets/nm_refresh_indicator.dart';
 
 import 'notes_view_model.dart';
 
